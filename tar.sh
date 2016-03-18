9bc65e47cb244eb9967bc51773558113a960eb9f
ezio <ezio.accton@gmail.com>
2016-03-18 17:41:50 +0800

    Add script for tar L4 folder

---
+++ tar.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd "/Users/swei/Documents/Work/code/"
+i=1
+NOW=$(date +"%Y_%m_%d")
+
+while [ $i -lt 100 ]; do
+    printf -v FILE 'l4_%s_%02d.tar.bz2' $NOW $i
+
+    if [ -f "$FILE" ]; then
+        ((i++))
+        continue
+    else
+        break
+    fi
+done
+
+#echo $FILE
+tar -cjf "$FILE" --exclude="*.git*" --exclude="*.DS_Store" ./l4
